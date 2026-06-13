89af8c14ad40fa88b6a90a57cc02194abbdbba73
Evan Haveman <evanhaveman@gmail.com>
2026-06-13 09:25:44 -0700

    bump sw

--- mondrian/sw.js
+++ mondrian/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mondrian-game-v5';
+const CACHE_NAME = 'mondrian-game-v6';
 const APP_SHELL = [
   './',
   './index.html',
--- recursive-truchet/sw.js
+++ recursive-truchet/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'recursive-truchet-game-v8';
+const CACHE_NAME = 'recursive-truchet-game-v9';
 const APP_SHELL = [
   './',
   './index.html',
